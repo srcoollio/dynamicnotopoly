684e5d99b148bb1d8120283053aef01af11b513a
some_weird_dude <patrick_boutros@hotmail.com>
2022-11-18 19:05:14 +0200

    added stimulus

---
+++ app/javascript/controllers/threejs_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="threejs"
+export default class extends Controller {
+  connect() {
+    console.log("test", this.element);
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "three", to: "https://ga.jspm.io/npm:three@0.146.0/build/three.module.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
