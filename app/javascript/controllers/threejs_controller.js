30b3ebf5cd76b63821fe7d706bf95863e7f60a8b
some_weird_dude <patrick_boutros@hotmail.com>
2022-11-25 17:45:22 +0200

    Initial sun creation(incomplete)

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -8,15 +8,22 @@ export default class extends Controller {
 		function addSphere(chosen_color) 
 		{
 			let geometry = new THREE.SphereGeometry( 1, 32, 16 );
-			let material = new THREE.MeshBasicMaterial( { color: chosen_color } );
+			let material = new THREE.MeshLambertMaterial( { color: chosen_color, wireframe:true } );
 			let sphere = new THREE.Mesh( geometry, material );
 			//group.add(sphere);
+			return sphere;
+		}
+		function addSun(chosen_color)
+		{
+			let geometry = new THREE.SphereGeometry( 1, 32, 16 );
+			let material = new THREE.MeshLambertMaterial( { color: chosen_color, wireframe:true, emissive: "red", emissiveIntensity: "40" } );
+			let sphere = new THREE.Mesh( geometry, material );
 			return sphere;
 		}
 		function basicRotation(Mesh ,x_value ,y_value)
 		{ 
 			Mesh.rotation.x += x_value;
-			//Mesh.rotation.y += y_value;
+			Mesh.rotation.y += y_value;
 		}
 		function BasicMovement(Mesh, speed)
 		{
@@ -25,13 +32,12 @@ export default class extends Controller {
 		function CircleMovement(Center, Mesh, speed, degree)
 		{
 			//0 = counter clockwise , 1 = clockwise
-			let offset = speed*degree;
+			let offset = speed*degree*1;
 			 let length_x = (Math.abs(Center.position.x) + Math.abs(Mesh.position.x));
 			 let length_y = (Math.abs(Center.position.y) + Math.abs(Mesh.position.y));
 			 length = Math.sqrt(Math.pow(length_x,2) + Math.pow(length_y, 2));
-			 //
-			console.log(length_x);
-			console.log(length_y);
+			//console.log(length_x);
+			//console.log(length_y);
 			 Mesh.position.x = Math.cos(offset)*length;
 			 Mesh.position.y = Math.sin(offset)*length;
 			return degree
@@ -117,10 +123,11 @@ export default class extends Controller {
 		document.body.appendChild( renderer.domElement );
 		const group = new THREE.Group();
 		const controls = new OrbitControls( camera, renderer.domElement );
-		
-		const sphere = addSphere("red");
+
+		const sphere = addSun("#ff1100");
 		sphere.position.set(0, 0, -2);
 		scene.add( sphere );
+
 
 		const sphere2  = addSphere("green");
 		sphere2.position.set(4, 0, -2);
